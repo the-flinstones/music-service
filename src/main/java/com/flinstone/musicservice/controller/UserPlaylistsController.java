d87c808d9c9b34f277f0da1234ab78a159687323
PratilipiAich <pratilipiaich98@gmail.com>
2021-01-16 22:40:06 +0530

    Modified user playlists controller

--- src/main/java/com/flinstone/musicservice/controller/UserPlaylistsController.java
+++ src/main/java/com/flinstone/musicservice/controller/UserPlaylistsController.java
@@ -1,68 +1,65 @@
-//package com.flinstone.musicservice.controller;
-//
-//import com.flinstone.musicservice.entity.CategoryEntity;
-//import com.flinstone.musicservice.entity.UserDetailsEntity;
-//import com.flinstone.musicservice.entity.UserPlaylistsEntity;
-//import com.flinstone.musicservice.service.UserDetailsService;
-//import com.flinstone.musicservice.service.UserPlaylistsService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-//import java.util.List;
-//
-//@CrossOrigin(origins = "*")
-//@RequestMapping("/api/v1")
-//@RestController
-//public class UserPlaylistsController {
-//    @Autowired
-//    private UserPlaylistsService userPlaylistsService;
-//
-//    //-----------------------------Create New Playlist---------------------------------------
-//    @PostMapping("/playlist")
-//    public UserPlaylistsEntity createPlaylist(@Valid @RequestBody UserPlaylistsEntity playlist) {
-//
-//        return userPlaylistsService.createPlaylist(playlist);
-//    }
-//
-//    //------------------------------Create All Playlist------------------------------------------
-//    @PostMapping("/playlists")
-//    List<UserPlaylistsEntity> createPlaylists(@RequestBody List<UserPlaylistsEntity> playlist){
-//
-//        return userPlaylistsService.createPlaylists(playlist);
-//    }
-//
-//    //-------------------------------Get Playlist By ID --------------------------------------------
-//    @GetMapping(value = "/playlist/{id}")
-//    public List<UserPlaylistsEntity> getPlaylistById(@PathVariable("id") String id) {
-//        return userPlaylistsService.getPlaylistsById(id);
-//    }
-//
-//    //-------------------------------Get All Playlists--------------------------------------------
-//    @GetMapping(value = "/playlists")
-//    public List<UserPlaylistsEntity> getAllPlaylists() {
-//
-//        return userPlaylistsService.getAllPlaylists();
-//    }
-//
-//    //-------------------------------Update Playlist By ID ----------------------------------------
-//    @PutMapping(value = "/playlist/{id}")
-//    public UserPlaylistsEntity updatePlaylistById(@PathVariable("id") String id, @RequestBody UserPlaylistsEntity playlist) {
-//
-//        return userPlaylistsService.updatePlaylistById(id, playlist);
-//    }
-//
-//    //---------------------------------Delete Playlist By ID ---------------------------------------
-//    @DeleteMapping(value = "/playlist/{id}")
-//    public String deletePlaylistById(@PathVariable("id") String id) {
-//
-//        userPlaylistsService.deletePlaylistById(id);
-//        return "Playlist with id " + id + " has been deleted!";
-//    }
-//
-//    //-------------------------------Get all songs from playlist-----------------------------------
-//    @GetMapping(value = "/playlist-songs/{userId}/{playlistId}")
-//    public List<UserPlaylistsEntity> getAllSongsByPlaylistIdUserId(@PathVariable("userId") String userId, String playlistId) {
-//        return  userPlaylistsService.getAllSongsByPlaylistIdUserId(userId, playlistId);
-//    }
-//}
+package com.flinstone.musicservice.controller;
+
+import com.flinstone.musicservice.entity.UserPlaylistsEntity;
+import com.flinstone.musicservice.service.UserPlaylistsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1")
+@RestController
+public class UserPlaylistsController {
+    @Autowired
+    private UserPlaylistsService userPlaylistsService;
+
+    //-----------------------------Create New Playlist---------------------------------------
+    @PostMapping("/playlist")
+    public UserPlaylistsEntity createPlaylist(@Valid @RequestBody UserPlaylistsEntity playlist) {
+
+        return userPlaylistsService.createPlaylist(playlist);
+    }
+
+    //------------------------------Create All Playlist------------------------------------------
+    @PostMapping("/playlists")
+    List<UserPlaylistsEntity> createPlaylists(@RequestBody List<UserPlaylistsEntity> playlist){
+
+        return userPlaylistsService.createPlaylists(playlist);
+    }
+
+    //-------------------------------Get Playlist By ID --------------------------------------------
+    @GetMapping(value = "/playlist/{id}")
+    public List<UserPlaylistsEntity> getPlaylistById(@PathVariable("id") String id) {
+        return userPlaylistsService.getPlaylistsById(id);
+    }
+
+    //-------------------------------Get All Playlists--------------------------------------------
+    @GetMapping(value = "/playlists")
+    public List<UserPlaylistsEntity> getAllPlaylists() {
+
+        return userPlaylistsService.getAllPlaylists();
+    }
+
+    //-------------------------------Update Playlist By ID ----------------------------------------
+    @PutMapping(value = "/playlist/{id}")
+    public UserPlaylistsEntity updatePlaylistById(@PathVariable("id") String id, @RequestBody UserPlaylistsEntity playlist) {
+
+        return userPlaylistsService.updatePlaylistById(id, playlist);
+    }
+
+    //---------------------------------Delete Playlist By ID ---------------------------------------
+    @DeleteMapping(value = "/playlist/{id}")
+    public String deletePlaylistById(@PathVariable("id") String id) {
+
+        userPlaylistsService.deletePlaylistById(id);
+        return "Playlist with id " + id + " has been deleted!";
+    }
+
+    //-------------------------------Get all songs from playlist-----------------------------------
+    @GetMapping(value = "/playlist-songs/{userId}/{playlistId}")
+    public List<UserPlaylistsEntity> getAllSongsByPlaylistIdUserId(@PathVariable("userId") String userId, String playlistId) {
+        return  userPlaylistsService.getAllSongsByPlaylistIdUserId(userId, playlistId);
+    }
+}
